51b72a7a6b75e0f42c450408807d412f284eaabe
Moses-amos <gamasebmoses2@gmail.com>
2020-08-13 19:38:39 +0200

    private info to dev.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-mongoose.connect('mongodb://127.0.0.1:27017/boilerplate', 
-{ useNewUrlParser: true, useUnifiedTopology: true})
+const config = require('./config/key')
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURL, 
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(() => console.log('DB connected'))
 .catch(err => console.log(err))
 
-const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.send('Hey')
+    res.json({"hey ~": "bla"})
 })
 
+app.post('/api/users/register', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send({user})
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 
 app.listen(5000, () => {
